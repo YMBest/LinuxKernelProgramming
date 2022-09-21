fbdde32a4f7d2af3a941caa19e23972ae42d0140
YMBest <105686494+YMBest@users.noreply.github.com>
2022-09-20 21:17:29 -0400

    Update setlxr.sh

--- setlxr.sh
+++ setlxr.sh
@@ -15,9 +15,23 @@ mv lxr-2.3.6 lxr
 cd lxr 
 mkdir linux-source
 cd linux-source
-tar zxf ~/Downloads/linux-6.0-rc6.tar.xz
+tar zxf ~/Downloads/linux-1.0.tar.xz
 cd ..
-mv linux-6.0-rc6 V6.0-rc6
+mv linux-1.0 V1.0
+
+./scripts/configure-lxr.pl -vv
+sudo ./ custom.d/initdb.sh
+cp custom.d/lxr.conf .
+
+./genxref --url=http://localhost/lxr --version=V1.0
+
+apache2ctl restart
+sudo cp custom.d/apache-lxrserver.conf /etc/apache2/conf-available
+sudo a2enconf apache-lxrserver.conf
+sudo a2dismod mpm*
+sudo apt-get install apache2-mpm-worker
+sudo a2enmod mpm_worker
+
 
 
 
